21f30f3f9fd6e9a6e97b682c0cb0a962fc6333af
Stephen Souness <stephensouness@outlook.com>
2022-12-06 15:21:43 +0000

    Task 5 View an Individual Review complete

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,45 @@
-function Reviews() {
-  return (
+import NavBar from "./NavBar";
+import SingleReview from "./SingleReview";
+import { getReviews } from "../api";
+import { useEffect, useState } from "react";
+import Button from "./Button";
+import "../App.css";
+
+function Contents() {
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getReviews().then((reviews) => {
+      setReviews(reviews);
+      setLoading(false);
+    });
+  }, []);
+
+  return loading ? (
+    <p>...page is loading</p>
+  ) : (
     <div>
-      <h2>Reviews:</h2>
+      <NavBar />
+
+      <ul className="reviewBox">
+        {reviews.map((review) => {
+          return (
+            <li key={review.review_id} className="reviewBox">
+              <h3>{review.title}</h3>
+              <p>Category: {review.category}</p>
+              <img src={review.review_img_url} alt={review.title} />
+              <p>Designer: {review.designer}</p>
+              <p>Votes: {review.votes}</p>
+              <Button reviewID={review.review_id} />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-export default Reviews;
+export default Contents;
+
+// pass this info down to Reviews when it works!!  (do it all in one place for now)
