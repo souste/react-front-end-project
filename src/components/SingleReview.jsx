78b768c7e14a470f70ab64df7089faa94c7dfd9d
Stephen Souness <stephensouness@outlook.com>
2022-12-09 15:27:25 +0000

    vote count applied, just need to fix one thing on dislike count

--- src/components/SingleReview.jsx
+++ src/components/SingleReview.jsx
@@ -6,10 +6,13 @@ import Comments from "./Comments";
 import "../App.css";
 import Button2 from "./Button2";
 
+import { patchVote } from "../api";
+
 function SingleReview() {
   const { review_id } = useParams();
   const [singleReview, setSingleReview] = useState({});
   const [loading, setLoading] = useState(true);
+  const [votes, setVotes] = useState(0);
 
   useEffect(() => {
     getReviewById(review_id).then((singleReview) => {
@@ -17,6 +20,30 @@ function SingleReview() {
       setLoading(false);
     });
   }, [review_id]);
+
+  const handleLikeVote = (review_id) => {
+    if (votes === 0) {
+      setVotes(1);
+      patchVote(review_id).then((review) => {
+        if (singleReview.review_id === review_id) {
+          return { ...review, votes: singleReview.votes + 1 };
+        }
+        return review;
+      });
+    }
+  };
+
+  const handleDislikeVote = (review_id) => {
+    if (votes === 0) {
+      setVotes(1);
+      patchVote(review_id).then((review) => {
+        if (singleReview.review_id === review_id) {
+          return { ...review, votes: singleReview.votes - 1 };
+        }
+        return review;
+      });
+    }
+  };
 
   return loading ? (
     <p>...page is loading</p>
@@ -34,7 +61,24 @@ function SingleReview() {
           <p>Owner: {singleReview.owner}</p>
           <p>Designer: {singleReview.designer}</p>
           <p>Created At: {singleReview.created_at}</p>
-          <p>Votes: {singleReview.votes}</p>
+          <div>
+            <button
+              onClick={() => {
+                handleLikeVote(singleReview.review_id);
+              }}
+            >
+              Like
+            </button>
+            <button
+              onCLick={() => {
+                handleDislikeVote(singleReview.review_id);
+              }}
+            >
+              Dislike
+            </button>
+
+            <p>Votes: {singleReview.votes + votes}</p>
+          </div>
         </li>
 
         <Comments />
